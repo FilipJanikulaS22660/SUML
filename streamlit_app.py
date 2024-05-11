afcd633ae88e9053f2ca4c670eab875b6b19bad3
Filip Janikula s22660 <149255804+FilipJanikulaS22660@users.noreply.github.com>
2024-05-11 19:36:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,13 @@
 import streamlit as st
 import time
+import streamlit as st
+from transformers import pipeline
+from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 # zaczynamy od zaimportowania bibliotek
 
 st.success('Gratulacje! Z powodzeniem uruchomiłeś aplikację')
-# streamlit jest wykorzystywany do tworzenia aplikacji
-# z tego powodu dobrą praktyką jest informowanie użytkownika o postępie, błędach, etc.
-
-# Inne przykłady do wypróbowania:
- # animowane balony ;)
-# st.error('Błąd!') # wyświetla informację o błędzie
-# st.warning('Ostrzeżenie, działa, ale chyba tak sobie...')
-# st.info('Informacja...')
-# st.success('Udało się!')
-
-# st.spinner()
-# with st.spinner(text='Pracuję...'):
-    # time.sleep(2)
-    # st.success('Done')
-# możemy dzięki temu "ukryć" późniejsze ładowanie aplikacji
 
 st.title('Zadanie streamlit SUML 🖥️')
-# title, jak sama nazwa wskazuje, używamy do wyświetlenia tytułu naszej aplikacji
-
-
-# header to jeden z podtytułów wykorzystywnaych w Streamlit
-
-# subheader to jeden z podtytułów wykorzystywnaych w Streamlit
-
-#st.text('To przykładowa aplikacja z wykorzystaniem Streamlit')
-# text używamy do wyświetlenia dowolnego tekstu. Można korzystać z polskich znaków.
-
-#st.write('Streamlit jest biblioteką pozwalającą na uruchomienie modeli uczenia maszynowego.')
-# write używamy również do wyświetlenia tekstu, różnica polega na formatowaniu.
-
-#st.code("st.write()", language='python')
-# code może nam się czasami przydać, jeżeli chcielibyśmy pokazać np. klientowi fragment kodu, który wykorzystujemy w aplikacji
-
-#with st.echo():
-#    st.write("Echo")
-# możemy też to zrobić prościej używając echo - pokazujemy kod i równocześnie go wykonujemy
-
-#df = pd.read_csv("DSP_4.csv", sep = ';')
-#st.dataframe(df)
-# musimy tylko pamiętać o właściwym określeniu separatora (w tym wypadku to średnik)
-# masz problem z otworzeniem pliku? sprawdź w jakim katalogu pracujesz i dodaj tam plik (albo co bardziej korzystne - zmień katalog pracy)
-# os.getcwd() # pokaż bieżący katalog
-# os.chdir("") # zmiana katalogu
 
 st.header('Przetwarzanie języka naturalnego')
 
@@ -55,9 +17,7 @@ st.write('Funkcja tłumaczenie korzysta z modelu Llama2-13b-Language-translate u
 
 st.write('Aby użyć opcji tłumaczenie należy z rozwijanego menu wybrać "Tłumaczenie angielskiego na niemiecki"')
 
-import streamlit as st
-from transformers import pipeline
-from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+
 
 def typewriter(text: str, speed: int):
                 tokens = text.split()
